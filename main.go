71fcde2ef399e877a842f761e49d2c237d762bbc
Andrés Felipe Méndez <fmendez1212@gmail.com>
2026-04-15 08:26:45 -0500

    nakama

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/heroiclabs/nakama-common/runtime"
+)
+
+//goland:noinspection GoUnusedExportedFunction
+func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
+	initStart := time.Now()
+
+	logger.Info("Module loaded in %dms", time.Since(initStart).Milliseconds())
+	return nil
+}
